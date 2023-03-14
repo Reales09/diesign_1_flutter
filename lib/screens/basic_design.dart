b1c491568ab0177ad64e7960c9244aa7dd3f0ed2
Reales <rclarence123@gmail.com>
2023-03-14 10:13:23 -0500

    Se grega botoes y texto para finalizar el diseño

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -10,7 +10,16 @@ class BasicDesignScreen extends StatelessWidget {
         children: [
           //landscape
           Image(image: AssetImage('assets/big-one.jpg')),
-          Title()
+          //titulo
+          Title(),
+          // Button Section
+          ButtonSection(),
+          //Description
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Text(
+                'Aliquip cupidatat culpa do deserunt dolore labore consequat mollit nulla commodo. Consequat amet commodo eiusmod ullamco consectetur eiusmod labore id magna ea reprehenderit cupidatat non minim. Labore anim excepteur pariatur et amet tempor est duis eu voluptate nisi anim. Laboris sint magna quis exercitation. Ea dolor laborum elit sint ex aliquip aute adipisicing Lorem labore duis anim. Ad dolor et officia sunt reprehenderit. Do do magna ipsum ipsum mollit enim laboris enim elit irure aute ea deserunt tempor.'),
+          )
         ],
       ),
     );
@@ -49,6 +58,60 @@ class Title extends StatelessWidget {
           Text('41')
         ],
       ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          CustomButton(
+            text: 'CALL',
+            icon: Icons.phone,
+          ),
+          CustomButton(text: 'ROUTE', icon: Icons.route),
+          CustomButton(
+            text: 'SHARE',
+            icon: Icons.share,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(
+          icon,
+          color: Colors.blue,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: Colors.blue),
+        )
+      ],
     );
   }
 }
